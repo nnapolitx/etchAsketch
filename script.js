9df0bfaaf8babea1822c3c210e3489505e7af680
nnapolitx <nnapolitano88@gmail.com>
2022-08-16 14:25:42 -0400

    blocks now wrap. May need to set up fn for width of blocks

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ grid16.addEventListener('click', grid16x16);
 
 function grid16x16() {
   
-  for (let i=0; i<= 15; i++) {
+  for (let i=1; i<= 16; i++) {
     const blocks = document.createElement('div');
     blocks.classList.add('.blocks');
     container.appendChild(blocks);
-    
+    blocks.textContent = 'A'
+    blocks.style.width = '25%';
   }
 
 }
