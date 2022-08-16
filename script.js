ffc6a05f552e186becf5843fa41ebe76c5c21f11
nnapolitx <nnapolitano88@gmail.com>
2022-08-16 14:21:04 -0400

    loop works, beginning to add style to blocks so that they wrap

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const grid16 = document.querySelector('.b16');
 
 grid16.addEventListener('click', grid16x16);
 
-
 function grid16x16() {
   
   for (let i=0; i<= 15; i++) {
-    console.log(i);
+    const blocks = document.createElement('div');
+    blocks.classList.add('.blocks');
+    container.appendChild(blocks);
+    
   }
 
 }
