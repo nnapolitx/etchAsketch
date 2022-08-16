e511a6a5a5a7d8188ba5f5ad5e14577a3cc04233
nnapolitx <nnapolitano88@gmail.com>
2022-08-16 14:16:41 -0400

    checking a function that uses a loop to create a grid

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('.container');
+const grid16 = document.querySelector('.b16');
+
+grid16.addEventListener('click', grid16x16);
+
+
+function grid16x16() {
+  
+  for (let i=0; i<= 15; i++) {
+    console.log(i);
+  }
+
+}
